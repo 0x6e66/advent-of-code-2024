4441d7de3d31d17033fb4675951fccd63369e544
Frondorf, Niklas <niklas@frondorf.eu>
2024-12-01 21:01:22 +0100

    day 01

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,5 +1,32 @@
-use advent_of_code_2024::{read_all, INPUT_PATH, TEST_INPUT_PATH};
+use advent_of_code_2024::INPUT_PATH;
 
 fn main() {
-    println!("day1 bin");
+    let lines = std::fs::read_to_string(INPUT_PATH).unwrap();
+
+    let (mut left, mut right): (Vec<i32>, Vec<i32>) = lines
+        .lines()
+        .filter_map(|line| line.split_once("   "))
+        .map(|(l, r)| (l.parse::<i32>().unwrap(), r.parse::<i32>().unwrap()))
+        .collect();
+
+    left.sort();
+    right.sort();
+
+    part1(&left, &right);
+    part2(&left, &right);
+}
+
+fn part1(left: &[i32], right: &[i32]) {
+    let sum: u32 = left.iter().zip(right).map(|(l, r)| l.abs_diff(*r)).sum();
+
+    println!("Part 1: {}", sum);
+}
+
+fn part2(left: &[i32], right: &[i32]) {
+    let sum: i32 = left
+        .iter()
+        .map(|l| right.iter().filter(|x| *x == l).count() as i32 * l)
+        .sum();
+
+    println!("Part 2: {}", sum);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ pub const TEST_INPUT_PATH: &str = "test_input.txt";
 
 pub fn read_all<T: FromStr>(file_name: &str) -> Vec<Result<T, <T as FromStr>::Err>> {
     std::fs::read_to_string(file_name)
-        .expect(format!("File '{file_name}' not found!").as_str())
+        .unwrap_or_else(|_| panic!("File '{file_name}' not found!"))
         .lines()
         .map(|x| x.parse())
         .collect()
